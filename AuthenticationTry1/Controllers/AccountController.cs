a0d31c48b1a79efb839dab652cff70b8f8d50473
Akhil <akhilmohan97@gmail.com>
2023-07-30 12:00:41 +0530

    Initail commit

---
+++ AuthenticationTry1/Controllers/AccountController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AuthenticationTry1.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public async Task<IActionResult> Register()
+        {
+            return View();
+        }
+    }
+}
---
+++ AuthenticationTry1/Models/RegisterViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AuthenticationTry1.Models
+{
+    public class RegisterViewModel
+    {
+        [Required]
+        [Display(Name ="Name")]
+        public string UserName { get; set; }
+        [EmailAddress]
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        [StringLength(25,ErrorMessage ="The {0} must be between {2} and {1} length",MinimumLength =5)]
+        public string Password { get; set; }
+        [Compare("Password",ErrorMessage ="Password does not match")]
+        public string ConfirmPassword { get; set; }
+    }
+}
