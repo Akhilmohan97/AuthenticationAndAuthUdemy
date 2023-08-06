d74c2132990f3ba20c9a77bed5458ff514e364ca
Akhil <akhilmohan97@gmail.com>
2023-08-06 19:57:42 +0530

    Registering view and user

--- AuthenticationTry1/Controllers/AccountController.cs
+++ AuthenticationTry1/Controllers/AccountController.cs
@@ -1,16 +1,35 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AuthenticationTry1.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 
 namespace AuthenticationTry1.Controllers
 {
     public class AccountController : Controller
     {
-        public IActionResult Index()
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly SignInManager<IdentityUser> _signInManager;
+        public AccountController(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager)
         {
-            return View();
+                _userManager = userManager;
+                _signInManager = signInManager;
         }
         [HttpGet]
         public async Task<IActionResult> Register()
         {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterViewModel registerViewModel)
+        {
+            if(ModelState.IsValid)
+            {
+                var user = new IdentityUser {Email=registerViewModel.Email,UserName=registerViewModel.UserName};
+               var registered= await _userManager.CreateAsync(user,registerViewModel.Password);
+                if(registered.Succeeded)
+                {
+                   await _signInManager.SignInAsync(user, isPersistent: false);
+                }
+            }
             return View();
         }
     }
